66c71f53ff62112338d7b7b46539550bb5460e3b
İsmail Konak <i_konak@hotmail.com>
2024-12-05 16:21:49 +0300

    Add files via upload

---
+++ Transformer-Circuits/Find-Median/model.py
@@ -0,0 +1,42 @@
+import torch as t
+import numpy as np
+from transformer_lens import HookedTransformer, HookedTransformerConfig
+
+
+def create_model(
+    max_num: int,
+    seq_len: int,
+    seed: int,
+    d_model: int,
+    d_head: int,
+    n_layers: int,
+    n_heads: int,
+    d_mlp: int | None,
+    normalization_type: str | None,
+    device: str = "cuda",
+    **kwargs,  # ignore other kwargs
+) -> HookedTransformer:
+    t.manual_seed(seed)
+    np.random.seed(seed)
+
+    attn_only = d_mlp is None
+
+    cfg = HookedTransformerConfig(
+        n_layers=n_layers,
+        n_ctx=seq_len,  
+        d_model=d_model,
+        d_head=d_head,
+        n_heads=n_heads,
+        d_mlp=d_mlp,
+        attn_only=attn_only,
+        act_fn="relu",
+        d_vocab=max_num,
+        use_attn_result=True,
+        use_split_qkv_input=True,
+        use_hook_tokens=True,
+        normalization_type=normalization_type,
+        device=device,
+    )
+
+    model = HookedTransformer(cfg)
+    return model
